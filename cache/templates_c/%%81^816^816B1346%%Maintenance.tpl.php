65d7f2f935c7e935484cb41e7cc50ee36ce17057
Your Name <you@example.com>
2019-01-16 17:59:11 +0700

    add gitignore

--- cache/templates_c/%%81^816^816B1346%%Maintenance.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.26, created on 2018-09-25 05:14:05
-         compiled from D:%5CCODE%5Ccode%5Cthuocdongy%5Ctemplates/Maintenance/Maintenance.tpl */ ?>
-<div class="body_content img-rounded">
-	<div class="container ">
-		<?php echo $this->_tpl_vars['Content']; ?>
-	
-	</div>
-</div>	
-
