27498984c3f124c0d9e03e21e57515655b528ada
Your Name <you@example.com>
2019-02-25 13:43:08 +0700

    update

--- webadmin.php
+++ webadmin.php
@@ -3,7 +3,10 @@ require_once 'core/Debug.php'; //System Debug...
 require_once 'core/config.php';//System Config...
 require_once 'core/Init.php';  //System Init...
 require_once 'core/config_admin.php';  
-
+$protocol = isset($_SERVER['HTTPS']) && (strcasecmp('off', $_SERVER['HTTPS']) !== 0);
+$hostname = $_SERVER['SERVER_ADDR'];
+$port = $_SERVER['SERVER_PORT'];
+if( $port !='8080') Url::redirect('/');
 CGlobal::$is_adminpage = true;
 if(CGlobal::$main == 'home') CGlobal::$main = 'admin_home';
 if(CGlobal::$main != 'admin_login') require_once('core/check_permission.php');
